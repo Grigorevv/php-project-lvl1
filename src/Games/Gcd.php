51799d04f96415dc09a2913dd5a21d4340f275af
Viktor Grigorev <7184243@gmail.com>
2021-10-23 18:21:04 +0300

    add brain-gcd

---
+++ src/Games/Gcd.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace src\Games\Gcd;
+
+use src\Engine;
+
+function getGcd($num1, $num2)
+{
+    $maxNumb = max($num1, $num2);
+    $gcd = 1;
+    for ($i = 2; $i <= $maxNumb; $i += 1) {
+        if ($num1 % $i === 0 && $num2 % $i === 0) {
+            $gcd = $i;
+        }
+    }
+    return $gcd;
+}
+
+function runGameEven()
+{
+    $gameDescription = 'Find the greatest common divisor of given numbers.';
+    $getCorrectAnswer = function () {
+        $num1 = rand(0, 100);
+        $num2 = rand(0, 100);
+        $question = "{$num1} {$num2}";
+        $correctAnswer = strval(getGcd($num1, $num2));
+        return [ 'question' => $question, 'correctAnswer' => $correctAnswer ];
+    };
+      Engine\engine($getCorrectAnswer, $gameDescription);
+}
