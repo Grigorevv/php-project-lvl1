5558821b795c2e8e0564eaf47cc9ebe65bb1db99
Viktor Grigorev <7184243@gmail.com>
2021-10-23 22:35:14 +0300

    add brain-prime

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -20,8 +20,8 @@ function runGameEven()
 {
     $gameDescription = 'Find the greatest common divisor of given numbers.';
     $getCorrectAnswer = function () {
-        $num1 = rand(0, 100);
-        $num2 = rand(0, 100);
+        $num1 = mt_rand(0, 100);
+        $num2 = mt_rand(0, 100);
         $question = "{$num1} {$num2}";
         $correctAnswer = strval(getGcd($num1, $num2));
         return [ 'question' => $question, 'correctAnswer' => $correctAnswer ];
---
+++ src/Games/Prime.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace src\Games\Prime;
+
+use src\Engine;
+
+function isPrime($num)
+{
+    if ($num < 2) {
+        return false;
+    }
+    for ($i = 2; $i <= $num / 2; $i += 1) {
+        if ($num % $i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function runGameEven()
+{
+    $gameDescription = 'Answer "yes" if the number is prime, otherwise answer "no".';
+    $getCorrectAnswer = function () {
+        $question = mt_rand(0, 100);
+        $correctAnswer = isPrime($question) ? 'yes' : 'no';
+        return [ 'question' => $question, 'correctAnswer' => $correctAnswer ];
+    };
+    Engine\engine($getCorrectAnswer, $gameDescription);
+}
